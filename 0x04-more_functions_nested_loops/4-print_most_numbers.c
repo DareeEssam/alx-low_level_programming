d18a5a24e79302fe0054fe5f130f61673dc60bec
DareeEssam <dareen2492002@gmail.com>
2023-03-23 04:43:04 -0700

    task4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -3,22 +3,22 @@
 /**
  * print_most_numbers - print numbers from 0 to 9
  * Description: Prints the numbers excluding 2 and 4
- * Return: The nubbers from 0 to 9
+ * Return: The numbers from 0 to 9
  */
 
 void print_most_numbers(void)
 {
 	int x = 0;
 
-	for (int x = 0; x <= 9; x++)
+	for (x = 0; x <= 9; x++)
 	{
-		if (x == 2 || x == 4)
-		{
-			continue;
-		}
-		else{
-			_putchar(x + '0');
-		}
+	if (x == 2 || x == 4)
+	{
+	continue;
+	}
+	else{
+	_putchar(x + '0');
+	}
 	}
 	_putchar('\n');
 }
