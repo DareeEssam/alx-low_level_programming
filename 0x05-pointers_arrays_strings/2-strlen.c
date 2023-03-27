a63e9229d314c35e44056c998e552f92c80fdf02
DareeEssam <dareen2492002@gmail.com>
2023-03-27 01:27:30 -0700

    task2

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -6,13 +6,16 @@
  * Return:length
  */
 
-int _strlen(char *s)
+int_strlen(char *s)
 {
 	int longi = 0;
-	while (s != '\0')
+	
+	while(*s != '\0')
 	{
 		longi++;
 		s++;
 	}
+
 	return (longi);
+
 }
