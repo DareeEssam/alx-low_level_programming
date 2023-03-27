75f72fdfe476a8994a61ee871f27316c4d552e79
DareeEssam <dareen2492002@gmail.com>
2023-03-27 01:59:11 -0700

    task11

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,8 +1,8 @@
 #include "time.h"
 
 /** 
- * main - program that generate random valid passwords for the program 101-crackme
- *
+ * main - program that generate random valid passwords
+ *for the program 101-crackme
  * Return: Always 0 (Success)
  */
 int main(void)
@@ -12,18 +12,18 @@ int main(void)
 
 	sum = 0;
 	
-	srand(time(0));
+	srand(time(NULL));
 
 	for (i = 0; i < 100; i++)
 	{
 		pass[i] = rand() % 78;
 		sum += (pass[i] + '0');
-		putchar(pass[i] + '0');
+		_putchar(pass[i] + '0');
 		if ((2772 - sum) - '0' < 78)
 		{
 			n = 2772 - sum - '0';
 			sum += n;
-			putchar(n + '0');
+			_putchar(n + '0');
 			break;
 		}
 	}
