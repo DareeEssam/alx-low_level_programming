e28fdcef2df0dd8bfae0fc2df8e414ec4b973896
DareeEssam <dareen2492002@gmail.com>
2023-04-03 18:33:17 -0700

    task6

--- 0x07-pointers_arrays_strings/_putchar.c
+++ 0x07-pointers_arrays_strings/_putchar.c
@@ -1,15 +1,13 @@
-#include "main.h"
 #include <unistd.h>
 
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to orint
+ * _putchar -  this writes is  the character c to stdout
+ * @c: The character to printer
  *
- * Return: On success 1
- * On error, -1 is returned
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
  */
 int _putchar(char c)
-
 {
 	return (write(1, &c, 1));
 }
