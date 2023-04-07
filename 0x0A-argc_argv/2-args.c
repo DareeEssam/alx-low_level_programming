b38719f07a837d4128807f3d430935ee062825d9
DareeEssam <dareen2492002@gmail.com>
2023-04-06 21:44:00 -0700

    task2

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - print all recieved numbers
+ * @argc: argumern number
+ * @argv: array of arguments
+ * Return: always 0 (success)
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s/n", argv[i]);
+	}
+
+	return (0);
+}
